da3693b965c9c06e07fa1806e5bb1e6b45bccc9f
Mikel Kulla <104671003+KullaMikel@users.noreply.github.com>
2022-05-06 19:28:38 +0200

    new excercise

---
+++ Beginning/ex 1/main.c
@@ -0,0 +1,15 @@
+//excercise to find perimeter and area of the rectangle
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,S,P;
+    a=7;
+    b=3;
+    S=a+b;
+    printf("Perimeter of rectangle is: %d",S);
+    P=a*b;
+    printf("\n\nArea of rectangle is: %d\n",P);
+}
